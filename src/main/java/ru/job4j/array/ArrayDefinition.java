0b53dc33d5e242fb8cb69284fece6c903a25d17a
Axiom <d-343@yandex.ru>
2023-07-07 19:40:07 +0300

    6.0.3. Заполнение массива. [#226300  [#226300]]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names =  new String[4];
+        names[0] = "Dmitry";
+        names[1] = "Anton";
+        names[2] = "Sveta";
+        names[3] = "Dasha";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
