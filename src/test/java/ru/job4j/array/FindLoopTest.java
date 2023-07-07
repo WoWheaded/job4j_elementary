e10d675a036866f00f3ac578e3db0adfd5b99aad
Axiom <d-343@yandex.ru>
2023-07-07 22:27:51 +0300

    6.1. Классический поиск перебором. [#33489  [#33489 #428565]]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FindLoopTest {
     @Test
     void whenArrayHas5Then0() {
-        int[] data = new int[] {5, 10, 3};
+        int[] data = new int[]{5, 10, 3};
         int el = 5;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
@@ -16,7 +16,7 @@ class FindLoopTest {
 
     @Test
     void whenArrayHasNot10ThenMinus1() {
-        int[] data = new int[] {5, 15, 3};
+        int[] data = new int[]{5, 15, 3};
         int el = 10;
         int result = FindLoop.indexOf(data, el);
         int expected = -1;
@@ -25,15 +25,16 @@ class FindLoopTest {
 
     @Test
     void whenArrayHas22Then4() {
-        int[] data = new int[] {123, 4213, 341, 212, 22, 34, 4543, 2268};
+        int[] data = new int[]{123, 4213, 341, 212, 22, 34, 4543, 2268};
         int el = 22;
         int result = FindLoop.indexOf(data, el);
         int expected = 4;
         assertThat(result).isEqualTo(expected);
     }
+
     @Test
     void whenArrayHas123Then0() {
-        int[] data = new int[] {123, 4213, 341, 212, 22, 34, 4543, 2268};
+        int[] data = new int[]{123, 4213, 341, 212, 22, 34, 4543, 2268};
         int el = 123;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
