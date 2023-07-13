edbab7dbdea415b009f3c450412e012dea597af6
Axiom <d-343@yandex.ru>
2023-07-13 12:57:35 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049  [#505049]]

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class SimpleStringEncoder {
+    public static String encode(String input) {
+        StringBuilder resultString = new StringBuilder();
+        char symbol = input.charAt(0);
+        int counter = 1;
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) != symbol) {
+                resultString.append(symbol).append(counter > 1 ? counter : "");
+                counter = 1;
+                symbol = input.charAt(i);
+                continue;
+            }
+            counter++;
+        }
+        return resultString.append(symbol).append(counter > 1 ? counter : "").toString();
+    }
+}
